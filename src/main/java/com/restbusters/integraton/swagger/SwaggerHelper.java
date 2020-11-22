a80ebc73856ef9e7ce7796c24ef0a1b4848c68a8
Alexander <alexander.matsaylo@gmail.com>
2020-11-22 12:37:12 -0800

    remove commented code

--- src/main/java/com/restbusters/integraton/swagger/SwaggerHelper.java
+++ src/main/java/com/restbusters/integraton/swagger/SwaggerHelper.java
@@ -102,59 +102,7 @@ public class SwaggerHelper {
         return swaggerDescriptors;
 
     }
-
-//    public void swaggerDiff2() throws Exception {
-//        TaskExecResult taskExecResult = setInitialTaskStatus("GENERATE_MANIFEST_DIFF");
-//        Response responseSourceOlder =
-//                RestClientHelper.getInstance().doGetRequest(okHttpClient, "https://tsm-leba.staging.ccs.guidewire.net/swagger-ui-v2/openapi.json", null, null);
-//        Response responseTargetNewer =
-//                RestClientHelper.getInstance().doGetRequest(okHttpClient, "https://tsm-leba.dev.ccs.guidewire.net/swagger-ui-v2/openapi.json", null, null);
-//
-//        if (responseSourceOlder.code() != 200 && responseTargetNewer.code() != 200) {
-//            logger.error("Failed to obtain swagger metadata, source code: {}, target  code {}: ", responseSourceOlder.code(), responseTargetNewer.code());
-//            Assert.fail("Failed to obtain swagger metadata, source or target swagger");
-//        }
-//
-//        Optional<String> jsonSource = convertYamlToJson(responseSourceOlder.body().string());
-//        Optional<String> jsonTarget = convertYamlToJson(responseTargetNewer.body().string());
-//        ObjectMapper jackson = new ObjectMapper();
-//        JsonNode beforeNode = jackson.readTree(jsonSource.get());
-//        JsonNode afterNode = jackson.readTree(jsonTarget.get());
-//        JsonNode patchNode = JsonDiff.asJson(beforeNode, afterNode);
-//        String diff = patchNode.toString();
-//        List<Map<String, Object>> diffObject = objectMapper.readValue(diff, new TypeReference<List<Map<String, Object>>>() {
-//        });
-//        String header = "| Where | Change | Value |\n";
-//        String subHeader = "| :--- | :--- | :--- |\n";
-//        StringBuilder diffBuilder
-//                = new StringBuilder(header);
-//        diffBuilder.append(subHeader);
-//        for (Map<String, Object> map : diffObject) {
-//
-//            if(!map.get("path").toString().contains("/description")){
-//                diffBuilder.append(String.format("| %s | %s | %s |\n", map.get("path"), map.get("op"), map.get("value")));
-//            }
-////            if(map.get("op").toString().equalsIgnoreCase("replace")){
-////                //buildJsonPath2(path);
-////                String jsonPath = buildJsonPath2(path);
-////                String versionOld = JsonPath.read(jsonSource.get() , jsonPath);
-////                String serviceName = JsonPath.read(jsonTarget.get(), jsonPath.replaceAll("version", "name"));
-////                logger.info("Service: {} previous version {} new version {}", serviceName, versionOld, map.get("value"));
-////                diffBuilder.append(String.format("| %s | %s | %s | %s |\n", serviceName, "updated", versionOld, map.get("value")));
-////            }
-//
-//        }
-//        logger.info(diffBuilder.toString());
-//    }
-
-//    public Map<String, List<com.restbusters.integration.swagger.model.SwaggerApiResource>> collectMultipleSwaggers(List<String> swaggerUrls){
-//        Map<String, >
-//        swaggerUrls.stream().parallel().forEach(object -> {
-//            //Your work on each object goes here, using object
-//        })
-//
-//
-//    }
+    
 
     private com.restbusters.integration.swagger.model.SwaggerApiResource createSwaggerApiResource(Operation operation, String resourcePath, String httpVerb) {
         com.restbusters.integration.swagger.model.SwaggerApiResource apiResource = new com.restbusters.integration.swagger.model.SwaggerApiResource();
