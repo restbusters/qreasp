7d308c6f458879c52312b1f3240d79cf56ca1b42
Alexander Matsaylo <softknife2021@gmail.com>
2025-10-11 15:23:59 -0700

    intitial working state for webdriver

--- src/main/java/com/restbusters/rest/client/RestClientHelper.java
+++ src/main/java/com/restbusters/rest/client/RestClientHelper.java
@@ -8,7 +8,6 @@ import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.annotation.Nullable;
 import javax.net.ssl.*;
 import java.io.IOException;
